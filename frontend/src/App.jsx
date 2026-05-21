a2eb7c833f6feec6fd454273555c15b108d0d41a
Hema Spandana <hemaspandana261@gmail.com>
2026-05-21 23:42:04 +0530

    pastel ui update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,9 @@ import UploadPanel from "./components/ui/UploadPanel"
 
 function App() {
   return (
-    <div className="relative flex bg-slate-950 text-white min-h-screen overflow-hidden">
-
-      <div className="absolute top-[-200px] left-[-100px] w-[500px] h-[500px] bg-cyan-500/10 blur-[120px] rounded-full" />
-
-      <div className="absolute bottom-[-200px] right-[-100px] w-[500px] h-[500px] bg-indigo-500/10 blur-[120px] rounded-full" />
-
+      <div className="relative flex bg-[#fffaf5] text-[#3b2f4a] min-h-screen overflow-hidden">   
+      <div className="absolute top-[-200px] left-[-100px] w-[500px] h-[500px] bg-pink-300/30 blur-[140px] rounded-full" />
+      <div className="absolute bottom-[-200px] right-[-100px] w-[500px] h-[500px] bg-purple-300/30 blur-[140px] rounded-full" />
       <Sidebar />
 
       <div className="flex-1 p-8 relative z-10">
